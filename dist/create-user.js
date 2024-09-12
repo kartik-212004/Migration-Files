3c579ccd6c7aba43189cc95e6883e914fb75d1f0
kartik <kartik200421@gmail.com>
2024-09-12 16:01:35 +0530

    checked ✅

--- dist/create-user.js
+++ dist/create-user.js
@@ -1,7 +1,24 @@
 "use strict";
-const a = {
-    name: "kartik",
-    class: "section B",
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-const { name } = a;
-console.log(name)
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield prisma.user.create({
+            data: {
+                email: "kartik200421@gmail.com",
+                name: "kartik",
+            },
+        });
+    });
+}
+main();
